aefa5b19e23c2258b87e27d9f0e230d66f4d3414
Keenah <kvancamp@mail.com>
2023-06-25 12:59:15 -0500

    sleep function

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var robot = require("robotjs");
 
-console.log("starting");
-robot.moveMouse(0, 0);
-console.log("done");
+function main() {
+  console.log("starting");
+  sleep(2000);
+  console.log("done");
+}
+
+function sleep(ms) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
+}
+main();
